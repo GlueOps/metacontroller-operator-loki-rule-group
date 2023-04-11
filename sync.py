9e93522c0c74864a1ae390c1a3b3b9744bc99a03
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2023-04-11 22:40:40 +0000

    feat: check that rule was created

--- sync.py
+++ sync.py
@@ -88,13 +88,15 @@ class LokiRuleGroupHandler(BaseHTTPRequestHandler):
                 self.wfile.write(json.dumps(response_data).encode('utf-8'))
             except Exception:
                 response_data = {
-                    "finalized": False
+                    "finalized": True
                 }
                 self.send_response(500)
                 self.send_header('Content-Type', 'application/json')
                 self.end_headers()
                 self.wfile.write(json.dumps(response_data).encode('utf-8'))
-                logger.exception(f'failed to delete rule group from request: {request_data}')
+                logger.exception(
+                    f'failed to delete rule group from request: {request_data}'
+                )
 
         else:
             # Sync the object with the external API
@@ -103,7 +105,10 @@ class LokiRuleGroupHandler(BaseHTTPRequestHandler):
                     rule_namespace=rule_group_namespace,
                     yaml_rule_group_definition=rule_group
                 )
-                if response.ok is True:
+                # if response.ok is True:
+                if rule_group == get_alerting_rules_in_namespace(
+                    rule_namespace=rule_group_namespace
+                ):
                     status = 'Healthy'
                 else:
                     status = 'Progressing'
